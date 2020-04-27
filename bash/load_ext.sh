61956d4e5025a0fb9afe1bf161f789b966b860e1
rwaweber <rwaweber@gmail.com>
2020-04-27 18:33:50 -0400

    add nvim and iron configs

--- bash/load_ext.sh
+++ bash/load_ext.sh
@@ -2,7 +2,7 @@
 
 if [ -f "$($HOME)/.bash-git-prompt/gitprompt.sh" ]; then
     echo "Git prompt installed"
-else 
-    cd ~
+else
+    cd ~ || exit
     git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt --depth=1
 fi
